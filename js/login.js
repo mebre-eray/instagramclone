db0b2c7b06427fea9cb3a9c9a9085623600306b7
mebre-eray <eraykalkan6@gmail.com>
2023-06-14 10:22:13 +0300

    login image setInterval

--- js/login.js
+++ js/login.js
@@ -2,8 +2,6 @@
 
 let loginBtn = document.getElementById("loginBtn")
 
-
-
 loginBtn.addEventListener("click", function() {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     let email = document.getElementById("email").value
@@ -15,3 +13,19 @@ loginBtn.addEventListener("click", function() {
     }
 
 })
+
+const images = ["/images/img1.png","/images/img2.png","/images/img3.png"];
+
+const imageScreen = document.getElementsByClassName("screen");
+
+let counter = 0;
+
+function handleChange() {
+
+    imageScreen[0].src = images[counter];
+    counter = (counter+1) % images.length;
+
+}
+
+setInterval(handleChange, 3000)
+ 
